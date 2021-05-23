47b7471c52aaec09fe82b0590b2270f5460072ef
Adalberto <adalbertoacarneiro@gmail.com>
2021-05-22 21:59:42 -0300

    Adição de imagens e criação da navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import StoreNavBar from '../components/StoreNavBar';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Game Store</title>
+        <meta name="description" content="Game Store Adalberto" />
+      </Head>
+      <StoreNavBar></StoreNavBar>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,6 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Game Store</title>
-        <meta name="description" content="Game Store Adalberto" />
-      </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>Game Store</h1>
       </main>
