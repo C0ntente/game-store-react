4148e98e40d1c9af1f60cd21c74749d35dd0aadf
Adalberto <adalbertoacarneiro@gmail.com>
2021-05-22 22:39:49 -0300

    Criação do ProductCard e busca dos produtos no Backend

--- pages/products.js
+++ pages/products.js
@@ -1,11 +1,32 @@
+import { Col, Container, Row } from 'react-bootstrap';
+import ProductCard from '../components/ProductCard';
 import styles from '../styles/Home.module.css';
 
-export default function Products() {
+export default function Products({ products }) {
   return (
-    <div>
+    <Container className="p-3">
+      <Row>
+        {products.map((product) => {
+          return (
+            <Col md="4" lg="3" className="pb-3" fluid="sm" key={product.id}>
+              <ProductCard product={product} />
+            </Col>
+          );
+        })}
+      </Row>
       <main className={styles.main}>
         <h1 className={styles.title}>Products</h1>
       </main>
-    </div>
+    </Container>
   );
+}
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:8080/products');
+  const products = await res.json();
+
+  return {
+    props: {
+      products,
+    },
+  };
 }
