ee77e6c94856b20d7c51adee6550e8785348914c
Adalberto <adalbertoacarneiro@gmail.com>
2021-05-31 22:05:06 -0300

    Adicionou o totalizador no carrinho

--- pages/cart.js
+++ pages/cart.js
@@ -1,21 +1,44 @@
+import Link from 'next/link';
 import React, { useContext } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { formatNumberAsCurrency } from '../commons/formatter';
 import CartItem from '../components/CartItem';
 import { CartContext } from '../contexts/CartContext';
 
 export default function cart() {
   const { cart } = useContext(CartContext);
-  return (
-    <Container className="p-3">
-      <Row>
-        {cart.map((cartItem) => {
-          return (
-            <Col sm="12" className="pb-3" key={cartItem.id}>
-              <CartItem cartItem={cartItem} />
-            </Col>
-          );
-        })}
-      </Row>
-    </Container>
-  );
+  function calculateCartTotal() {
+    let total = 0;
+    cart.forEach((product) => {
+      total += product.quantity * product.price;
+    });
+    return total;
+  }
+  if (cart.length > 0) {
+    return (
+      <Container className="p-3">
+        <Row>
+          {cart.map((cartItem) => {
+            return (
+              <Col sm="12" className="pb-3" key={cartItem.id}>
+                <CartItem cartItem={cartItem} />
+              </Col>
+            );
+          })}
+        </Row>
+        <Row>
+          <div className="justify-content-end d-flex">
+            <h3>Total: {formatNumberAsCurrency(calculateCartTotal())}</h3>
+          </div>
+        </Row>
+        <Row>
+          <Link href="checkout">
+            <Button variant="primary">Checkout</Button>
+          </Link>
+        </Row>
+      </Container>
+    );
+  } else {
+    return <h1>Carrinho vazio</h1>;
+  }
 }
