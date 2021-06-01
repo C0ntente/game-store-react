b5b7a8746d14f0e629ba8b1e6abca26123c9a82a
Adalberto <adalbertoacarneiro@gmail.com>
2021-05-31 22:05:31 -0300

    Adicionou os botões de remover e adicionar produto

--- components/CartItem.js
+++ components/CartItem.js
@@ -1,14 +1,27 @@
+import React, { useContext } from 'react';
+import { Button } from 'react-bootstrap';
+import { CartContext } from '../contexts/CartContext';
 import { formatNumberAsCurrency } from '../commons/formatter';
 export default function CartItem({ cartItem }) {
+  const { removeProductFromCart, addProductToCart } = useContext(CartContext);
+
   return (
     <div className="d-flex justify-content-between">
       <h1>{cartItem.name}</h1>
       <div>
-        <Button size="sm" className="me-2">
+        <Button
+          size="sm"
+          className="me-2"
+          onClick={() => removeProductFromCart(cartItem)}
+        >
           -
         </Button>
         <span>{cartItem.quantity}</span>
-        <Button size="sm" className="ms-2">
+        <Button
+          size="sm"
+          className="ms-2"
+          onClick={() => addProductToCart(cartItem)}
+        >
           +
         </Button>
         <span className="ms-2">
